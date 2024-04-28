9ad2fc0fda2c49cd33b67becf87731614a017ec0
Cami C.A <camic.a@Cami-2.local>
2024-04-28 13:40:44 -0400

    working on adding the api endpoints and returning it when the server runs

--- api/views.py
+++ api/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def getRoutes(request):
+
+    routes = [
+        {
+            'Endpoint': '/blissfullden/',
+            'method': 'GET',
+            'body': None,
+            'description': "Returns an array of notes"
+        },
+        {
+            'Endpoint': '/blissfullden/id/',
+            'method': 'GET',
+            'body': None,
+            'description': "Returns a single note"
+        },
+        {
+            'Endpoint': '/blissfullden/create/',
+            'method': 'POST',
+            'body': {'Body': ""},
+            'description': "Creates a new note with data sent in post request"
+        },
+        {
+            'Endpoint': '/blissfullden/id/update/',
+            'method': 'PUT',
+            'body': {'Body':""},
+            'description': "Creates an existing note with data sent in put"
+        },
+        {
+            'Endpoint': '/blissfullden/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': "Deletes and existing note"
+        }
+    ]
+
+    return JsonResponse(routes, safe=False)
