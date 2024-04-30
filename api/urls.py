ab3f090664db24d07f9b92f870edc63881bab566
Cami C.A <camic.a@Cami-2.local>
2024-04-29 21:23:36 -0400

    finished creatinh api endpoints

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes),
-    path('tasks/', views.getTask)
-]
+    path('tasks/', views.getTasks),
+    path('tasks/create/', views.createTask),
+    path('tasks/<str:pk>/update/', views.updateTask),
+    path('tasks/<str:pk>/delete/', views.deleteTask),
+    path('tasks/<str:pk>/', views.getTask) 
+    ]
