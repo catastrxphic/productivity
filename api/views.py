0c4e5b63c735f7cb17e387aad376435b5d9a4145
Cami C.A <camic.a@Cami-2.local>
2024-05-03 10:26:12 -0400

    log in and sign up python logic

--- api/views.py
+++ api/views.py
@@ -1,9 +1,35 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import TaskSerializer
 from .models import Task
+
 from rest_framework.authtoken.models import Token
 from .serializers import UserSerializer
+from rest_framework import status
+from django.contrib.auth.models import User
+
+@api_view (['POST'])
+def login(request):
+    user= get_object_or_404(User, username= request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+    return Response({"token":token.key, "user": serializer.data})
+
+@api_view (['POST'])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username= request.data['username'])
+        user.set_password(request.data['password']) # hashing password
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({"token":token.key, "user":serializer.data})
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET'])
 def getRoutes(request):
