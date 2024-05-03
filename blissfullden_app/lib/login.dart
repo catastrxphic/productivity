f08764863e26b0ffed5087ae49d31427eadd3a79
Cami C.A <camic.a@Cami-2.local>
2024-05-03 15:55:38 -0400

    working on sign up

--- blissfullden_app/lib/login.dart
+++ blissfullden_app/lib/login.dart
@@ -1,5 +1,8 @@
+import 'package:blissfullden_app/components/button.dart';
 import 'package:blissfullden_app/components/my_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 
 
 class LoginPage extends StatelessWidget {
@@ -7,6 +10,26 @@ class LoginPage extends StatelessWidget {
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
 
+  Future <void> login(BuildContext context) async{
+    final response = await http.post(
+      Uri.parse("http://127.0.0.1:8000/login/"),
+      body:{
+        'username':usernameController.text,
+        'password':passwordController.text,
+      },
+    );
+
+    if (response.statusCode == 200){
+      Navigator.pushNamed(context, '/tasks/');
+    }
+    else{
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content:  Text('Login failed.')
+          ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +68,47 @@ class LoginPage extends StatelessWidget {
                 controller: passwordController,
                 hintText: 'password' ,
                 obscureText: true,
+              ),
+
+              const SizedBox(height: 10),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Color.fromARGB(255, 131, 198, 237)),
+                    )
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 25),
+              Button(onTap:()=> login(context)),
+
+              const SizedBox(height: 50),
+              GestureDetector(
+                onTap:(){
+                  Navigator.pushNamed(context, '/signup');
+                } ,
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Not a member?',
+                    style: TextStyle(color:Colors.grey)
+                    ,),
+                    SizedBox(width: 4),
+                    Text(
+                      "Register now",
+                      style: TextStyle(
+                      color:Colors.blue, fontWeight: FontWeight.bold
+                      ),
+                    )
+                  ],
+                )
               )
+              
             ],
           ),
         )
