e361f6c0a3c11010a417a0981712f61642b44304
cami <mcopo001@fiu.edu>
2024-12-19 21:20:31 -0500

    log in and register done, front and back end wise

--- blissfullden_app/lib/pages/profile/profile_page.dart
+++ blissfullden_app/lib/pages/profile/profile_page.dart
@@ -1,14 +1,72 @@
+import 'package:blissfullden_app/pages/profile/login_page.dart';
+import 'package:blissfullden_app/pages/profile/register_page.dart';
+import 'package:blissfullden_app/pages/profile/user_info_page.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key?key}):super(key:key);
 
   @override
-  Widget build (BuildContext context){
-    return const Scaffold(
-      body:Center(
-        child: Text("page name")
-      )
+  State <ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+
+  bool isLoggedIn = false;
+
+  @override
+  void initState(){
+    super.initState();
+    checkLoginStatus();
+  }
+
+  Future<void> checkLoginStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isLoggedIn = prefs.getBool('isLoggedIn')?? false;
+    });
+  }
+
+ @override
+ Widget build(BuildContext context){
+  return Scaffold(
+    body: isLoggedIn ? const UserInfoPage() : const AuthChooserPage(),
+  );
+ }
+}
+
+class AuthChooserPage extends StatelessWidget{
+
+  const AuthChooserPage({Key?key}): super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+              child: const Text('Log In'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => RegisterPage()),
+                );
+              },
+              child: const Text('Register'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
