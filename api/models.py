ba5c2b6d7ddad62704c4fa3acf924357f6b40999
cami <mcopo001@fiu.edu>
2024-12-21 18:32:59 -0500

    Updated database, now to update code

--- api/models.py
+++ api/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 class Task(models.Model):
-    body = models.TextField()
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=50, choices= [
+        ('pending', "Pending"),
+        ('in_progress', "In Progress"),
+        ('completed', "Completed")
+    ])
+    additional_notes = models.CharField(max_length=500, blank=True, null=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
