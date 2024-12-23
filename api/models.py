be911dffc25b9a784e72a5850b64643c2e29ea5d
cami <mcopo001@fiu.edu>
2024-12-23 16:19:09 -0500

    changed db and pages

--- api/models.py
+++ api/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks", null=True)
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
@@ -14,7 +17,7 @@ class Task(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) :
-        return self.body[:50]
+        return self.title
     
     class Meta:
         ordering = ['-updated']
--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,24 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('tasks', views.TaskViewSet, basename='task')
+
+# urlpatterns = [
+#     path('', views.getRoutes),
+#     path('tasks/', views.getTasks),
+#     path('tasks/create/', views.createTask),
+#     path('tasks/<str:pk>/update/', views.updateTask),
+#     path('tasks/<str:pk>/delete/', views.deleteTask),
+#     path('tasks/<str:pk>/', views.getTask),
+#     path('signup/', views.signup),
+#     path('login/', views.login),
+#     ]
+
+from .views import TaskViewSet
 
 urlpatterns = [
-    path('', views.getRoutes),
-    path('tasks/', views.getTasks),
-    path('tasks/create/', views.createTask),
-    path('tasks/<str:pk>/update/', views.updateTask),
-    path('tasks/<str:pk>/delete/', views.deleteTask),
-    path('tasks/<str:pk>/', views.getTask),
     path('signup/', views.signup),
     path('login/', views.login),
-    ]
+] + router.urls
